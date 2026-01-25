480d043a31027a165b0ee0410b316463b9b4545d
Sijan666 <666majharulislam@gmail.com>
2026-01-25 23:53:58 +0600

    making services part

---
+++ src/components/layouts/Service.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Container from '../Container'
+import Flex from '../Flex'
+
+const Service = () => {
+    return (
+        <>
+        <div className="py-25 bg-[#EAEDF2]">
+            <Container>
+                <div className="mb-10">
+                    <h3 className='text-[#090A0C] font-bold text-[35px] uppercase'>
+                        <span className='border-b-2 border-[#D9832E] pb-1'>My</span> Services
+                    </h3>
+                    <p className='text-[14px] text-[#555A64] tracking-[2px] uppercase pt-2.5'>
+                        A Lead UX & UI designer based in Canada
+                    </p>
+                </div>
+                <div className="">
+                    <Flex>
+                        
+                    </Flex>
+                </div>
+            </Container>
+        </div>
+        </>
+    )
+}
+
+export default Service
