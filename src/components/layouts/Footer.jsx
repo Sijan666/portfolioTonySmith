c46ec14de8a6b2ebb8d35b766cae8b68e9626499
Sijan666 <666majharulislam@gmail.com>
2026-05-27 21:57:09 +0600

    fix responsive issue in  footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -5,23 +5,21 @@ import { BsInstagram, BsTwitter } from "react-icons/bs"
 import { LiaLinkedin } from "react-icons/lia"
 import { PiPinterestLogo } from "react-icons/pi"
 
-
-
 const Footer = () => {
   return (
     <>
-    <div className="py-5.5 bg-[#111111]">
-      <Container>
-        <Flex className={'justify-between'}>
+    <div className="py-5 md:py-5.5 bg-[#111111]">
+      <Container className={'px-4 lg:px-0'}>
+        <Flex className={'flex-col md:flex-row justify-center md:justify-between items-center gap-y-6 md:gap-y-0'}>
           <div className="flex gap-x-5">
-            <FaFacebook className="text-white/50 cursor-pointer"/>
-            <BsTwitter className="text-white/50 cursor-pointer"/>
-            <BsInstagram className="text-white/50 cursor-pointer"/>
-            <LiaLinkedin className="text-white/50 cursor-pointer"/>
-            <PiPinterestLogo className="text-white/50 cursor-pointer"/>
+            <FaFacebook className="text-white/50 hover:text-white transition-colors duration-300 cursor-pointer text-lg md:text-base"/>
+            <BsTwitter className="text-white/50 hover:text-white transition-colors duration-300 cursor-pointer text-lg md:text-base"/>
+            <BsInstagram className="text-white/50 hover:text-white transition-colors duration-300 cursor-pointer text-lg md:text-base"/>
+            <LiaLinkedin className="text-white/50 hover:text-white transition-colors duration-300 cursor-pointer text-lg md:text-base"/>
+            <PiPinterestLogo className="text-white/50 hover:text-white transition-colors duration-300 cursor-pointer text-lg md:text-base"/>
           </div>
-          <div className="">
-            <p className="text-white/50 text-[12px]">© 2019 copyright all right reserved</p>
+          <div className="text-center md:text-left">
+            <p className="text-white/50 text-[12px] md:text-[14px]">© 2026 copyright all right reserved</p>
           </div>
         </Flex>
       </Container>
