22f9be1fd800dfc3296b50f8c6dc6c6ed609653b
Sijan666 <666majharulislam@gmail.com>
2026-05-27 22:21:49 +0600

    update testimonial

--- src/components/layouts/Testimonials.jsx
+++ src/components/layouts/Testimonials.jsx
@@ -17,7 +17,7 @@ const Testimonials = () => {
             <Container>
                 <div className="mb-10">
                     <h3 className='text-[#090A0C] font-bold text-[35px] uppercase'>
-                        <span className='border-b-2 border-[#D9832E] pb-1'>My</span> Services
+                        <span className='border-b-2 border-[#D9832E] pb-1'>My</span> Clients
                     </h3>
                     <p className='text-[14px] text-[#555A64] tracking-[2px] uppercase pt-2.5'>
                         A Lead UX & UI designer based in Canada
