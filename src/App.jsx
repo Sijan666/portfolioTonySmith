721a39136ac27bfa130b87d6b1aed69808d3f5b8
Sijan666 <666majharulislam@gmail.com>
2026-06-08 21:35:33 +0600

    remove unwanted components and add react lenis for premium scrolling behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import './App.css'
-import Home from './components/pages/Home'
-import { Routes , Route } from 'react-router-dom'
-import RootLayouts from './components/layouts/RootLayouts'
+import './App.css';
+import Home from './components/pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import RootLayouts from './components/layouts/RootLayouts';
+import Error from './components/pages/Error';
+import ReactLenis from 'lenis/react';
 
-
-
-function App () {
+function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          {/* <Route path="/about" element={<About/>} /> */}
-        </Route>
-          {/* <Route path="*" element={<Error/>} /> */}
-      </Routes>
+      <style>{`
+        html.lenis, html.lenis body {
+          height: auto;
+        }
+        .lenis.lenis-smooth {
+          scroll-behavior: auto !important;
+        }
+        .lenis.lenis-smooth [data-lenis-prevent] {
+          overscroll-behavior: contain;
+        }
+        .lenis.lenis-stopped {
+          overflow: hidden;
+        }
+        .lenis.lenis-smooth iframe {
+          pointer-events: none;
+        }
+      `}</style>
+
+      <ReactLenis root options={{ lerp: 0.08, duration: 1.5, smoothWheel: true, smoothTouch: false,}}>
+        <Routes>
+          <Route path="/" element={<RootLayouts />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </ReactLenis>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
